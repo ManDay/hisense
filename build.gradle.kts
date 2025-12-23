898ab81d0c9b20c9d0a478c8ef5b29d3bd68c0ef
Cedric Sodhi <manday@openmail.cc>
2025-12-23 19:30:10 +0100

    Fixing

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ android {
     defaultConfig {
         applicationId = "com.hisense.einkservice"
         minSdk = 30
-        targetSdkVersion = 34
+        targetSdki = 34
         versionCode = 7
         versionName = "1.6"
         vectorDrawables {
