9d3695e4f9cc09db36e4b8dde2c4a416fb28c873
Cedric Sodhi <manday@openmail.cc>
2025-12-24 15:36:21 +0100

    Fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
-    implementation("androidx.activity:activity-compose:1.12.2")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.10.0")
     implementation("androidx.compose.ui:ui-graphics:1.10.0")
     implementation("androidx.compose.ui:ui-tooling:1.10.0")
