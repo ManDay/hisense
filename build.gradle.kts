8798131bacd568e32922fb9d590b35a98845744d
Cedric Sodhi <manday@openmail.cc>
2025-12-24 15:00:42 +0100

    Bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ android {
         dataBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
@@ -30,13 +30,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.compose.ui:ui:1.6.6")
-    implementation("androidx.compose.ui:ui-graphics:1.6.6")
-    implementation("androidx.compose.ui:ui-tooling:1.6.6")
+    implementation("androidx.compose.ui:ui:1.10.0")
+    implementation("androidx.compose.ui:ui-graphics:1.10.0")
+    implementation("androidx.compose.ui:ui-tooling:1.10.0")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.35.0-alpha")
     implementation("androidx.navigation:navigation-compose:2.7.0")
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-ktx:2.8.4")
 }
