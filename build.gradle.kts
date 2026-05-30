b4545b48ce38a69c103a7c88659de9a62839acaf
Cedric Sodhi <manday@openmail.cc>
2026-05-30 22:23:34 +0200

    KAPT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   id("com.android.application") version "8.13.2"
   id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
   id("org.jetbrains.kotlin.android") version "2.2.0"
-  id("org.jetbrains.kotlin.kapt") version "2.8.4"
+  id("org.jetbrains.kotlin.kapt") version "2.2.0"
 }
 
 kotlin {
