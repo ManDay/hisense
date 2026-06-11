8ddf66136ea473f0c5595754a332c9bebe4c1b7e
Cedric Sodhi <manday@openmail.cc>
2026-06-11 18:17:42 +0200

    Serv

--- src/main/cpp/a9srv.c
+++ src/main/cpp/a9srv.c
@@ -4,23 +4,92 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Socket Protocol
+ *
+ * <id>=<value> Sets a value
+ * <id> Gets a value
+ * <id>! Toggles a value
+ */
+
+#define COUNT( array ) ( sizeof( array )/sizeof( array[ 0 ] ) )
+#define REDUCE( type,var,src,n ) type var = 0; memcpy( &var,src,( n )<sizeof( type ) ? ( n ) : sizeof( type ) )
+#define ASSERT0( val ) assert( ( val )!= -1 )
 
 #define SOCKET_NAME "\0testsocket"
 #define BUFFER_SIZE 512
+#define DISP_BASE "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/"
 
+//#define DISP_MODE ( DISP_BASE "epd_display_mode" )
+//#define DISP_CLEAR ( DISP_BASE "epd_force_clear" )
+//#define LED_BASE "/sys/class/backlight/aw99703-bl-"
+//#define LED_BRIGHTNESS "/brightness"
+
+#define DISP_MODE "/tmp/disp_mode"
+#define DISP_CLEAR "/tmp/disp_clear"
+#define LED_BASE "/tmp/led"
+#define LED_BRIGHTNESS "bright"
+
+#define LED_PATH( yellow ) ( yellow ? ( LED_BASE "1" LED_BRIGHTNESS ) : ( LED_BASE "2" LED_BRIGHTNESS ) )
+
+const short epd_mode_ids[ ]= { 515,513,518,521 };
+ 
 void write_file( const char* const f,const char* const s,size_t l ) {
  int fd;
- assert( ( fd = open( f,O_WRONLY ) )!= -1 );
- assert( write( fd,s,l )!= -1 );
+ ASSERT0( ( fd = open( f,O_WRONLY ) ) );
+ ASSERT0( write( fd,s,l ) );
  close( fd );
 }
 
-void epd_write( unsigned code ) {
- char s[3];
- sprintf( s,"%u",code );
- write_file( "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/epd_display_mode",
-             s,
-             sizeof( s ) );
+void read_file( const char* const f,char* const s,size_t l ) {
+ int fd;
+ ASSERT0( ( fd = open( f,O_RDONLY ) ) );
+ ASSERT0( read( fd,s,l ) );
+ close( fd );
+}
+
+void epd_write( char mode ) {
+ short code = epd_mode_ids[ mode % COUNT( epd_mode_ids ) ];
+
+ char s[4];
+ snprintf( s,sizeof( s ),"%hi\n",code );
+ write_file( DISP_MODE,s,sizeof( s ) );
+}
+
+char epd_read( ) {
+ char s[4];
+ 
+ read_file( DISP_MODE,s,sizeof( s ) );
+ s[ 3 ]= '\0';
+ long l = strtol( s,NULL,0 );
+ 
+ for( char i = 0; i<COUNT( epd_mode_ids ); i++ )
+  if( l == epd_mode_ids[ i ] )
+   return i;
+ 
+ return 0;
+}
+
+void epd_clear( ) {
+ write_file( DISP_CLEAR,"1",1 );
+}
+
+void led_write( bool yellow,char val ) {
+ char s[4];
+ write_file( LED_PATH( yellow ),s,snprintf( s,sizeof( s ),"%hi\n",val ) );
+} 
+
+char led_read( bool yellow ) {
+ char s[4];
+ 
+ read_file( LED_PATH( yellow ),s,sizeof( s ) );
+ long l = strtol( s,NULL,0 );
+ 
+ char result = l;
+ 
+ return result;
 }
 
 int main( void ) {
@@ -29,30 +98,48 @@ int main( void ) {
  
  struct sockaddr_un server_addr = { AF_UNIX,SOCKET_NAME };
 
- assert( ( sock = socket( AF_UNIX,SOCK_DGRAM,0 ) )>= 0 );
- assert( bind( sock,
+ ASSERT0( sock = socket( AF_UNIX,SOCK_SEQPACKET,0 ) );
+ ASSERT0( bind( sock,
          (struct sockaddr*)&server_addr,
-         offsetof( struct sockaddr_un,sun_path )+sizeof( SOCKET_NAME )-1 )>= 0 );
+         offsetof( struct sockaddr_un,sun_path )+sizeof( SOCKET_NAME )-1 ) );
+ ASSERT0( listen( sock,8 ) );
  
- while (1) {
-  ssize_t i = recv( sock,buffer,BUFFER_SIZE-1,0 );
+ while( true ) {
+  int cmdstream = accept( sock,NULL,NULL );
   
-  if( i>0 ) {
+  size_t i;
+  while( ( i = read( cmdstream,buffer,BUFFER_SIZE-1 ) )> 0 ) {
    buffer[ i ]= '\0';
    char cmd = buffer[ 0 ];
+   char* args = i>1 ? buffer + 1 : NULL;
    
-   char* args = buffer + 1;
-
-   if( cmd == 'c' )
-    epd_write( 515 );
-   else if( cmd == 'b' )
-    epd_write( 513 );
-   else if( cmd == 's' )
-    epd_write( 518 );
-   else if( cmd == 'p' )
-    epd_write( 521 );
+   if( cmd == 'm' )
+    if( args ) {
+     REDUCE( char,val,args,i - 1 );
+     epd_write( val );
+    } else {
+     char val = epd_read( );
+     ASSERT0( write( cmdstream,&val,sizeof( epd_read ) ) );
+    }
+   else if( cmd == 'c' )
+    epd_clear( );
+   else {
+    bool white = cmd == 'w';
+    if( white || cmd == 'y' )
+     if( args ) {
+      REDUCE( char,val,args,i - 1 );
+      led_write( white,val );
+     } else {
+      char val = led_read( white );
+      ASSERT0( write( cmdstream,&val,sizeof( led_read ) ) );
+     }
+    else
+     fprintf( stderr,"Did not understand '%c'\n",cmd ); 
+   }
   }
+  
+  close( cmdstream );
  }
-
+ 
  return 0;
 }
--- src/main/kotlin/com/hisense/einkservice/services/IEinkServiceInterfaceImpl.kt
+++ src/main/kotlin/com/hisense/einkservice/services/IEinkServiceInterfaceImpl.kt
@@ -6,7 +6,6 @@ import android.net.LocalSocketAddress
 import com.hisense.einkservice.IEinkServiceInterface
 import com.hisense.einkservice.model.EinkSpeed
 import com.hisense.einkservice.observers.NightLightIntensityObserver
-import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
@@ -14,82 +13,80 @@ class IEinkServiceInterfaceImpl : IEinkServiceInterface.Stub() {
     private val TAG = IEinkServiceInterfaceImpl::class.java.getSimpleName()
     private val SRV_SOCKET = "testsocket"
     
-    private val EINK_PATH = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/"
-    private val YELLOW_LED = "/sys/devices/platform/soc/4a84000.i2c/i2c-1/1-0036/backlight/aw99703-bl-1/brightness"
-    private val WHITE_LED = "/sys/devices/platform/soc/4a80000.i2c/i2c-0/0-0036/backlight/aw99703-bl-2/brightness"
-    
-    fun sendCommand( cmd: String ) {
-     val socket = LocalSocket( LocalSocket.SOCKET_DGRAM )
+    fun srvSet( tgt: Byte,v: ByteArray ) {
+     val socket = LocalSocket( LocalSocket.SOCKET_SEQPACKET )
      socket.connect( LocalSocketAddress( SRV_SOCKET ) )
-     socket.outputStream.write( cmd.toByteArray() )
+     socket.outputStream.write( tgt + v )
      socket.outputStream.flush( )
      socket.close( )
     }
+    
+    fun srvGet( tgt: Byte,r: ByteArray ) {
+     val socket = LocalSocket( LocalSocket.SOCKET_SEQPACKET )
+     socket.connect( LocalSocketAddress( SRV_SOCKET ) )
+     socket.outputStream.write( tgt )
+     socket.outputStream.flush( )
+     r?.let { socket.inputStream.read( r ) }
+     socket.close( )
+    }
 
-    override fun setSpeed(speed: Char) {
+    override fun setSpeed(speed: Int) {
         Log.i(TAG, "setting speed mode: $speed")
-        sendCommand( speed.toString() )
+        srvSet( 'm'.code.toByte( ),speed.toUByte( ) );
     }
 
     override fun clearScreen() {
         Log.i(TAG, "clearing screen")
-        sendCommand( "r" )
+        srvGet( 'c',null );
     }
 
-    override fun getCurrentSpeed(): Char {
+    override fun getCurrentSpeed(): Int {
         val speed = readFromFile(EINK_PATH + "epd_display_mode")
-        return EinkSpeed.fromInt( speed.filter { it.isDigit() }.toInt() ).toChar( )
+        
+        r = ByteArray(1)
+        srvGet( 'm'.code.toByte( ),r )
+        
+        return r[0].toInt()
     }
 
     override fun setTemperature(isNightLight: Boolean, brightness: Int) {
         val originalScale = NightLightIntensityObserver.originalScale(brightness)
         if (isNightLight) {
             setNightLight(true)
-            sendCommand( "y" + originalScale.toString() )
+//            sendCommand( "y" + originalScale.toString() )
         } else {
             setNightLight(false)
-            sendCommand( "w" + originalScale.toString() )
+//            sendCommand( "w" + originalScale.toString() )
         }
     }
 
     private fun setNightLight(enabled: Boolean) {
         if (enabled) {
-            sendCommand( "w" + 0.toString() )
+//            sendCommand( "w" + 0.toString() )
         } else {
-            sendCommand( "y" + 0.toString() )
+//            sendCommand( "y" + 0.toString() )
         }
     }
 
     override fun isNightLight(): Boolean {
-        val whiteBrightness = readFromFile(WHITE_LED).filter { it.isDigit() }.toInt()
+        /*val whiteBrightness = readFromFile(WHITE_LED).filter { it.isDigit() }.toInt()
         val yellowBrightness = readFromFile(YELLOW_LED).filter { it.isDigit() }.toInt()
-        return whiteBrightness == 0 && yellowBrightness > 0
+        return whiteBrightness == 0 && yellowBrightness > 0*/
+        return false
     }
 
     override fun getBrightness(): Int {
-        val whiteBrightness = readFromFile(WHITE_LED).filter { it.isDigit() }.toInt()
+        /*val whiteBrightness = readFromFile(WHITE_LED).filter { it.isDigit() }.toInt()
         val yellowBrightness = readFromFile(YELLOW_LED).filter { it.isDigit() }.toInt()
         return if (whiteBrightness == 0) {
             yellowBrightness
         } else {
             whiteBrightness
-        }
+        }*/
+        return 0
     }
 
     override fun setLockedScreen(lockscreen: CharArray?) {
         // TODO
-    }
-
-    private fun readFromFile(
-        filename: String,
-    ): String {
-        try {
-            val file = File(filename)
-            val stream = file.inputStream()
-            return stream.bufferedReader().use { it.readText() }
-        } catch (e: IOException) {
-            Log.e(TAG, "File read failed: $e")
-        }
-        return ""
     }
 }
