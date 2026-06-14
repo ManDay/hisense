b74cdf32dc6194f6981110becb54306a23d02b99
Cedric Sodhi <manday@openmail.cc>
2026-06-14 12:19:10 +0200

    asd

--- src/main/kotlin/com/hisense/einkservice/ui/views/EinkMainActivityScreen.kt
+++ src/main/kotlin/com/hisense/einkservice/ui/views/EinkMainActivityScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
